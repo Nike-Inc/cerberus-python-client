9b664b534e42d9ea0cef324186e91f5885ca83c8
Wallace, Ann <Ann.Wallace@nike.com>
2017-02-19 17:53:41 -0800

    moved user auth to it's own class

--- cerberus/client.py
+++ cerberus/client.py
@@ -4,6 +4,8 @@ import requests
 import sys
 import boto3
 
+from user_auth import UserAuth
+
 class CerberusClient(object):
     HEADERS = {"Content-Type": "application/json"}
 
@@ -20,7 +22,8 @@ class CerberusClient(object):
 
     def set_token(self):
         if self.username:
-            self.token =  self.get_user_token()
+            ua = UserAuth(self.cerberus_url, self.username, self.password)
+            self.token =  ua.get_token()
         else:
             self.set_aws_auth()
             self.token =  self.get_iam_role_token()
@@ -30,28 +33,9 @@ class CerberusClient(object):
      #       print("ERROR: No auth set")
      #       sys.exit(2)
 
-    def get_user_token(self):
-        """sets client token from Cerberus"""
-        auth_resp = self.get_auth()
-        if auth_resp['status'] == 'mfa_req':
-            token_resp = self.get_mfa(auth_resp)
-        else:
-            token_resp = auth_resp
-        token = token_resp['data']['client_token']['client_token']
-        return token
-
     def get_token(self):
         """Returns a client token from Cerberus"""
         return self.token
-
-    def get_auth(self):
-        """Returns auth respose which has client token unless MFA is required"""
-        auth_resp = requests.get(self.cerberus_url + '/v2/auth/user',
-                                 auth=(self.username, self.password))
-        auth_resp_json = json.loads(auth_resp.text)
-        if auth_resp.status_code != 200:
-           auth_resp.raise_for_status()
-        return auth_resp_json
 
     def set_aws_auth(self):
         """Sets the variables needed for AWS Auth"""
@@ -87,20 +71,6 @@ class CerberusClient(object):
         token = json.loads( response['Plaintext'].decode('utf-8'))['client_token']
         return token
 
-    def get_mfa(self, auth_resp):
-        """Gets MFA code from user and returns response which includes the client token"""
-        # TODO check if there is more than 1 device.
-        # currently cerberus only support Google Authenicator
-        sec_code = input('Enter ' + auth_resp['data']['devices'][0]['name'] + ' security code: ')
-        mfa_resp = requests.post(self.cerberus_url + '/v2/auth/mfa_check',
-                                json={'otp_token': sec_code,
-                                      'device_id': auth_resp['data']['devices'][0]['id'],
-                                      'state_token': auth_resp['data']['state_token']},
-                                       headers=self.HEADERS)
-        mfa_resp_json = json.loads(mfa_resp.text)
-        if mfa_resp.status_code != 200:
-            mfa_resp.raise_for_status()
-        return mfa_resp_json
 
     def get_sdb_path(self,sdb):
         """Returns the path for a SDB"""
