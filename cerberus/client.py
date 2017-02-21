1d20a821e070bd4ffb2b429212f1f41707bc5592
Wallace, Ann <Ann.Wallace@nike.com>
2017-02-20 18:17:46 -0800

    added aws_auth

--- cerberus/client.py
+++ cerberus/client.py
@@ -1,10 +1,10 @@
-import base64
 import json
 import requests
 import sys
-import boto3
 
+# local classes
 from .user_auth import UserAuth
+from .aws_auth import AWSAuth
 
 class CerberusClient(object):
     HEADERS = {"Content-Type": "application/json"}
@@ -13,64 +13,21 @@ class CerberusClient(object):
         self.cerberus_url = cerberus_url
         self.username = username or ""
         self.password = password or ""
-        # aws creds
-        self.account_id = None
-        self.role_name = None
-        self.region = None
         self.token = None
         self.set_token()
 
     def set_token(self):
+        """set the token based on auth type"""
         if self.username:
             ua = UserAuth(self.cerberus_url, self.username, self.password)
             self.token =  ua.get_token()
         else:
-            self.set_aws_auth()
-            self.token =  self.get_iam_role_token()
-     #   elif self.account_id is not None:
-     #       return self.get_iam_role_token()
-     #   else:
-     #       print("ERROR: No auth set")
-     #       sys.exit(2)
+            awsa = AWSAuth(self.cerberus_url)
+            self.token =  awsa.get_token()
 
     def get_token(self):
         """Returns a client token from Cerberus"""
         return self.token
-
-    def set_aws_auth(self):
-        """Sets the variables needed for AWS Auth"""
-        boto = boto3.client('sts')
-        self.account_id = boto.get_caller_identity().get('Account')
-        self.role_name = boto.get_caller_identity().get('Arn').split('/')[1]
-        # get the region - couldn't figure out how to do this with boto3
-        response = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
-        self.region = json.loads(response.text)['region']
-
-    def get_iam_role_token(self):
-        """Returns a clinet token from Cerberus"""
-        request_body = {
-            'account_id': self.account_id,
-            'role_name': self.role_name,
-            'region': self.region
-        }
-
-        encrypted_resp = requests.post(self.cerberus_url + '/v1/auth/iam-role',
-                                        data=json.dumps(request_body))
-        encrypted_resp_json = json.loads(encrypted_resp.text)
-        if encrypted_resp.status_code != 200:
-           encrypted_resp.raise_for_status()
-
-        auth_data = encrypted_resp_json['auth_data']
-
-        client = boto3.client('kms', region_name=self.region)
-
-        response = client.decrypt(
-            CiphertextBlob=base64.decodebytes(bytes(auth_data, 'utf-8'))
-        )
-
-        token = json.loads( response['Plaintext'].decode('utf-8'))['client_token']
-        return token
-
 
     def get_sdb_path(self,sdb):
         """Returns the path for a SDB"""
