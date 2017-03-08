7855aca7082fb4ae94ea9db2602e628015d85927
Wallace, Ann <Ann.Wallace@nike.com>
2017-03-07 18:10:59 -0800

    updated setup for packaging

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ with open('requirements.txt') as f:
 
 setup(
     name='cerberus python client',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=requirements,
     author='Ann Wallace',
     author_email='ann.wallace@nike.com',
-    description="A python client for interacting with Cerberus and Valut",
+    description="A python client for interacting with Cerberus and Vault",
+    url='https://github.com/Nike-Inc/cerberus-python-client',
+    license='Apache License, v2.0',
+    keywords='cerberus nike',
     packages=find_packages(exclude=('tests', 'docs')),
     test_suite="tests",
   )
