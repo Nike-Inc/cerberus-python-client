a0ee40a1edeffdbcec8b3faec081d88e8af0be0f
JT Cray <Jackson.Cray@nike.com>
2017-11-20 16:52:02 -0800

    Updating cerberus client to support more functions
    Added code for creating safety deposit boxes
    along with adding code that makes it easier to inspect roles.
    Code to delte secrets has also been added.
    
    Extended unit testing
    
    Updated Changelog and rev'd version
    Added requirements.txt file
    
    Updated README.md with more examples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to `Cerberus Python Client` will be documented in this file.
 
 ### Added
 
+###### 28-11-2017
+- Version 0.7.0 - Extend library to support creation, updates, and deletion of saftey deposit boxes and secrets.  Added some convience functions for getting and displaying information about an sdb, roles, and categories. 
+
 ###### 27-02-2017
 - Added the ability for setting role ARN and region that differs from default for IAM role auth.
 
--- cerberus/client.py
+++ cerberus/client.py
@@ -19,6 +19,8 @@ from requests.exceptions import RequestException
 from .aws_auth import AWSAuth
 from .user_auth import UserAuth
 from . import CerberusClientException, CLIENT_VERSION
+import ast
+import json
 
 
 class CerberusClient(object):
@@ -39,8 +41,7 @@ class CerberusClient(object):
         self.set_token()
 
         self.HEADERS['X-Vault-Token'] = self.token
-        self.HEADERS['X-Cerberus-Client'] = 'CerberusPythonClient/' \
-                                                + CLIENT_VERSION
+        self.HEADERS['X-Cerberus-Client'] = 'CerberusPythonClient/' + CLIENT_VERSION
 
     def set_token(self):
         """Set the Vault token based on auth type"""
@@ -54,6 +55,104 @@ class CerberusClient(object):
     def get_token(self):
         """Returns a client token from Cerberus"""
         return self.token
+
+    def get_roles(self):
+        """Returns all the roles (IAM or User Groups) that can be granted to a safe deposit box.
+
+        Roles are permission levels that are granted to IAM or User Groups.  Associating the id for the write role
+          would allow that IAM or User Group to write in the safety deposit box."""
+        sdb_resp = requests.get(self.cerberus_url + '/v1/role',
+                                headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+        return sdb_resp.json()
+
+    def get_role(self, key):
+        """Returns id of named role."""
+
+        sdb_resp = requests.get(self.cerberus_url + '/v1/role',
+                                headers=self.HEADERS)
+        self.throw_if_bad_response(sdb_resp)
+        json_resp = sdb_resp.json()
+        for item in json_resp:
+            if key in item["name"]:
+                return item["id"]
+        raise CerberusClientException("Key '%s' not found" % key)
+
+    def list_roles(self):
+        """Simplified version of get_roles that returns a dict of just name: id for the roles"""
+
+        json_resp = self.get_roles()
+        temp_dict = {}
+        for item in json_resp:
+            temp_dict[item["name"]] = item["id"]
+        return temp_dict
+
+    def get_categories(self):
+        """ Return a list of categories that a safe deposit box can belong to"""
+        sdb_resp = requests.get(self.cerberus_url + '/v1/category',
+                                headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+        return sdb_resp.json()
+
+    def create_sdb(self, name, category_id, owner, description="", user_group_permissions=None,
+                   iam_principal_permissions=None):
+        """Create a safety deposit box.
+
+        You need to get a new token before the iam role is granted permission to the new safety deposit box.
+        Keyword arguments:
+            name (string) -- name of the safety deposit box
+            category_id (string) -- category id that determines where to store the sdb. (ex: shared, applications)
+            owner (string) -- AD group that owns the safety deposit box
+            description (string) -- Description of the safety deposit box
+            user_group_permissions (list) -- list of dictionaries containing the key name and maybe role_id
+            iam_principal_permissions (list) -- list of dictionaries containing the key name iam_principal_arn
+            and role_id
+        """
+
+        # Do some sanity checking
+        if user_group_permissions is None:
+            user_group_permissions = []
+        if iam_principal_permissions is None:
+            iam_principal_permissions = []
+        if list != type(user_group_permissions):
+            raise(TypeError('Expected list, but got ' + str(type(user_group_permissions))))
+        if list != type(iam_principal_permissions):
+            raise(TypeError('Expected list, but got ' + str(type(iam_principal_permissions))))
+        temp_data = {
+            "name": name,
+            "description": description,
+            "category_id": category_id,
+            "owner": owner,
+        }
+        if len(user_group_permissions) > 0:
+            temp_data["user_group_permissions"] = user_group_permissions
+        if len(iam_principal_permissions) > 0:
+            temp_data["iam_principal_permissions"] = iam_principal_permissions
+        data = json.encoder.JSONEncoder().encode(temp_data)
+        sdb_resp = requests.post(self.cerberus_url + '/v2/safe-deposit-box', data=str(data), headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+        return sdb_resp.json()
+
+    def delete_sdb(self, sdb_id):
+        """ Delete a safety deposit box specified by id
+
+        Keyword arguments:
+        sdb_id -- this is the id of the saftey deposit box, not the path."""
+        sdb_resp = requests.delete(self.cerberus_url + '/v2/safe-deposit-box/' + sdb_id,
+                                   headers=self.HEADERS)
+        self.throw_if_bad_response(sdb_resp)
+        return sdb_resp
+
+    def get_sdbs(self):
+        """ Return a list of each SDB the client is authorized to view"""
+        sdb_resp = requests.get(self.cerberus_url + '/v2/safe-deposit-box',
+                                headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+        return sdb_resp.json()
 
     def get_sdb_path(self, sdb):
         """Returns the path for a SDB"""
@@ -79,7 +178,10 @@ class CerberusClient(object):
         return list_resp.json()['data']['keys']
 
     def get_sdb_id(self, sdb):
-        """ Return the ID for the given safety deposit box"""
+        """ Return the ID for the given safety deposit box.
+
+        Keyword arguments:
+        sdb -- This is the name of the saftey deposit box, not the path"""
         sdb_resp = requests.get(self.cerberus_url + '/v1/safe-deposit-box',
                                 headers=self.HEADERS)
 
@@ -92,6 +194,82 @@ class CerberusClient(object):
         # If we haven't returned yet then we didn't find what we were
         # looking for.
         raise CerberusClientException("'%s' not found" % sdb)
+
+    def get_sdb_id_by_path(self, sdb_path):
+        """ Given the path, return the ID for the given safety deposit box."""
+        sdb_resp = requests.get(self.cerberus_url + '/v2/safe-deposit-box',
+                                headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+
+        # Deal with the supplied path possibly missing an ending slash
+        path = sdb_path
+        if not sdb_path.endswith('/'):
+            path = sdb_path + '/'
+
+        for r in sdb_resp.json():
+            if r['path'] == path:
+                return str(r['id'])
+
+        # If we haven't returned yet then we didn't find what we were
+        # looking for.
+        raise CerberusClientException("'%s' not found" % sdb_path)
+
+    def get_sdb_details(self, sdb_id):
+        """ Return the details for the given safety deposit box id
+
+        Keyword arguments:
+        sdb_id -- this is the id of the saftey deposit box, not the path."""
+        sdb_resp = requests.get(self.cerberus_url + '/v2/safe-deposit-box/' + sdb_id,
+                                headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+
+        return sdb_resp.json()
+
+    def update_sdb(self, sdb_id, owner=None, description=None, user_group_permissions=None,
+                   iam_principal_permissions=None):
+        """Update a safety deposit box.
+
+        Keyword arguments:
+
+            owner (string) -- AD group that owns the safety deposit box
+            description (string) -- Description of the safety deposit box
+            user_group_permissions (list) -- list of dictionaries containing the key name and maybe role_id
+            iam_principal_permissions (list) -- list of dictionaries containing the key name iam_principal_arn
+            and role_id
+        """
+        # Grab current data
+        old_data = self.get_sdb_details(sdb_id)
+
+        # Assemble information to update
+        temp_data = {}
+        keys = ('owner', 'description','iam_principal_permissions', 'user_group_permissions')
+        for k in keys:
+            if k in old_data:
+                temp_data[k] = old_data[k]
+        if owner is not None:
+            temp_data["owner"] = owner
+        if description is not None:
+            temp_data["description"] = description
+        if user_group_permissions is not None and len(user_group_permissions) > 0:
+            temp_data["user_group_permissions"] = user_group_permissions
+        if iam_principal_permissions is not None and len(iam_principal_permissions) > 0:
+            temp_data["iam_principal_permissions"] = iam_principal_permissions
+
+        data = json.encoder.JSONEncoder().encode(temp_data)
+        sdb_resp = requests.put(self.cerberus_url + '/v2/safe-deposit-box/' + sdb_id, data=str(data),
+                                headers=self.HEADERS)
+
+        self.throw_if_bad_response(sdb_resp)
+        return sdb_resp.json()
+
+    def delete_secret(self, vault_path):
+        """Delete a secret from the given vault path"""
+        secret_resp = requests.delete(self.cerberus_url + '/v1/secret/' + vault_path,
+                                      headers=self.HEADERS)
+        self.throw_if_bad_response(secret_resp)
+        return secret_resp
 
     def get_secret(self, vault_path, key):
         """Returns the secret based on the vault_path and key"""
@@ -110,6 +288,56 @@ class CerberusClient(object):
         self.throw_if_bad_response(secret_resp)
 
         return secret_resp.json()
+
+    def list_secrets(self, vault_path):
+        """Returns json secrets based on the vault_path, this will list keys in a folder"""
+        secret_resp = requests.get(self.cerberus_url + '/v1/secret/' + vault_path + '?list=true',
+                                   headers=self.HEADERS)
+        self.throw_if_bad_response(secret_resp)
+        return secret_resp.json()
+
+    def put_secret(self, vault_path, key, merge=True):
+        """Write secret(s) to a vault_path provided a dictionary of key/values
+
+        Keyword arguments:
+        vault_path -- full path in the secret deposit box that contains the key
+        key -- A dictionary containing key/values to be written at the vault_path
+        merge -- Boolean that determines if the provided key should be merged with
+            the values already present at the vault_path.  If False the key will
+            completely overwrite what was stored at the vault_path. (default True)
+        """
+        # json encode the input.  Cerberus is sensitive to double vs single quotes.
+        # an added bonus is that json encoding transforms python2 unicode strings
+        # into a compatible format.
+        data = json.encoder.JSONEncoder().encode(key)
+        if merge:
+            data = self.secret_merge(vault_path, key)
+        secret_resp = requests.put(self.cerberus_url + '/v1/secret/' + vault_path,
+                                   data=str(data), headers=self.HEADERS)
+        self.throw_if_bad_response(secret_resp)
+        return secret_resp
+
+    def secret_merge(self, vault_path, key):
+        """Compares key/values at vault_path and merges them.  New values will overwrite old."""
+        get_resp = requests.get(self.cerberus_url + '/v1/secret/' + vault_path, headers=self.HEADERS)
+        temp_key = {}
+        # Ignore a return of 404 since it means the key might not exist
+        if get_resp.status_code == requests.codes.bad and get_resp.status_code not in [403, 404]:
+            self.throw_if_bad_response(get_resp)
+        elif get_resp.status_code in [403, 404]:
+            temp_key = {}
+        else:
+            temp_key = get_resp.json()['data']
+        # Allow key to be either a string describing a dict or a dict.
+        if type(key) == str:
+            temp_key.update(ast.literal_eval(key))
+        else:
+            temp_key.update(key)
+        # This is a bit of a hack to get around python 2 treating unicode strings
+        # differently.  Cerberus will throw a 400 if we try to post python 2 style
+        # unicode stings as the payload.
+        combined_key = json.encoder.JSONEncoder().encode(temp_key)
+        return combined_key
 
     def throw_if_bad_response(self, response):
         """Throw an exception if the Cerberus response is not successful."""
