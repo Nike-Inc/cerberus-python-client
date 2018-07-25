b9d6b9ca503e4fdbb2a23177a4b76a8916a821f1
slin38 <sean.lin@nike.com>
2018-07-24 17:37:37 -0700

    * fix a bug where html error message would blow up the client

--- cerberus/util.py
+++ cerberus/util.py
@@ -1,3 +1,19 @@
+
+"""
+Copyright 2018-present Nike, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and* limitations under the License.*
+"""
+
 from requests.exceptions import RequestException
 from . import CerberusClientException
 import json
@@ -11,9 +27,13 @@ def throw_if_bad_response(response):
     """Throw an exception if the Cerberus response is not successful."""
     try:
         response.raise_for_status()
-    except RequestException as e:
-        msg = 'Response code: {}; response body:\n{}'.format(response.status_code, json.dumps(response.json(), indent=2))
-        raise CerberusClientException(msg)
+    except RequestException:
+        try:
+            msg = 'Response code: {}; response body:\n{}'.format(response.status_code, json.dumps(response.json(), indent=2))
+            raise CerberusClientException(msg)
+        except ValueError:
+            msg = 'Response code: {}; response body:\n{}'.format(response.status_code, response.text)
+            raise CerberusClientException(msg)
 
 
 def get_with_retry(url, retry=default_retry_attempt_number, **kwargs):
