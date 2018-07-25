2133c8fe1230fcee2f90c47b4447b28af26981ea
Sean Lin <linxiao_e@hotmail.com>
2018-07-25 10:33:51 -0700

    * fix a bug where html error message would blow up the client (#34)

---
+++ tests/test_util.py
@@ -0,0 +1,45 @@
+"""
+Copyright 2018-present Nike, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and* limitations under the License.*
+"""
+
+import json
+import unittest
+
+import requests
+
+from cerberus.util import throw_if_bad_response
+from cerberus import CerberusClientException
+
+
+class TestUtil(unittest.TestCase):
+    """Mock unit test for util"""
+
+    @staticmethod
+    def _mock_response(status=200, reason=None, content=''):
+        mock_resp = requests.Response()
+        mock_resp.status_code = status
+        # Reason the status code occurred.
+        mock_resp.reason = reason
+        # Raw content in byte
+        mock_resp._content = bytes(content.encode('utf-8'))
+        return mock_resp
+
+    def test_html(self):
+        resp = self._mock_response(status=403, content='<html>403 Unauthorized</html>')
+        self.assertRaises(CerberusClientException, throw_if_bad_response, resp)
+
+    def test_json(self):
+        error_json = { 'error_id': '1'}
+        resp = self._mock_response(status=403, content=json.dumps(error_json))
+        self.assertRaises(CerberusClientException, throw_if_bad_response, resp)
