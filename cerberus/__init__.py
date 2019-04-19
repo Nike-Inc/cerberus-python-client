e225aae84dcd2414422f2df00a3d03abcda5e611
JT Cray <Jackson.Cray@nike.com>
2019-04-19 15:57:11 -0700

    Changing __init__.py to be more readable

--- cerberus/__init__.py
+++ cerberus/__init__.py
@@ -1,9 +1,6 @@
 # Setting up logging here because this is the root of the module
 import logging
 
-# This avoids the "No handler found" warnings.
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
 __all__ = ['client', 'user_auth', 'aws_auth', 'util']
 
 CLIENT_VERSION = '2.2.0'
@@ -11,3 +8,7 @@ CLIENT_VERSION = '2.2.0'
 class CerberusClientException(Exception):
     """Wrap third-party exceptions expected by the Cerberus client."""
     pass
+
+# This avoids the "No handler found" warnings.
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
